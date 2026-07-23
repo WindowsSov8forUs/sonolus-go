c0c6640accf819841800c759a96088c1b8f34055
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2026-07-23 14:28:08 +0800

    test(compiler): 覆盖完整固定对象图组合

--- internal/compiler/testdata/persistentglobals/play.go
+++ internal/compiler/testdata/persistentglobals/play.go
@@ -23,6 +23,20 @@ type persistentPackageHolder struct{ Input persistentAutoInput }
 
 var packageHolder = &persistentPackageHolder{Input: &persistentAutoInputImpl{Bias: 5}}
 
+type persistentPackageInput struct {
+	TempPair *persistentPair
+	Units    [2]persistentUnit
+	Auto     persistentAutoInput
+	Current  int
+}
+
+var packageInput = &persistentPackageInput{
+	TempPair: &persistentPair{},
+	Units:    [2]persistentUnit{{Value: 1}, {Value: 2}},
+	Auto:     &persistentAutoInputImpl{Bias: 3},
+	Current:  4,
+}
+
 func (pair *persistentPair) Set(unit, other *persistentUnit) *persistentPair {
 	pair.Unit = unit
 	pair.Other = other
@@ -76,6 +90,13 @@ type PersistentNote struct {
 }
 
 func (*PersistentNote) Preprocess() {
+	if packageInput.TempPair == nil || packageInput.Auto == nil || packageInput.Units[1].Value != 2 || packageInput.Current != 4 {
+		sonolus.Terminate("package pointer-rich graph was not initialized")
+	}
+	packageInput.TempPair.Set(&packageInput.Units[1], nil)
+	if packageInput.Auto.Apply(packageInput.TempPair.Unit.Value) != 5 {
+		sonolus.Terminate("package pointer-rich graph dispatch failed")
+	}
 	if packageHolder.Input == nil || packageHolder.Input.Apply(1) != 6 {
 		sonolus.Terminate("package initial interface graph was not initialized")
 	}
